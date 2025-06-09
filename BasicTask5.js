fde302e66f6f9877717389d2febf6fc932439dd2
Pathan Ayan khan <ayankhan782204@gmail.com>
2025-06-09 12:17:51 +0530

    Create BasicTask5.js

---
+++ BasicTask5.js
@@ -0,0 +1,13 @@
+ function greetUser(name) {
+  console.log("Welcome, " + name + "!");
+}
+
+function square(number) {
+  return number * number;
+}
+
+
+greetUser("Ayan"); // You can replace "Asad" with any name
+
+let result = square(5); // You can replace 5 with any number
+console.log("Square: " + result);
