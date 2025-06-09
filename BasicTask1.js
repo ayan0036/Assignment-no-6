5c844329586135569198920ba5deaa416bd940de
Pathan Ayan khan <ayankhan782204@gmail.com>
2025-06-09 12:10:00 +0530

    Create BasicTask1.js

---
+++ BasicTask1.js
@@ -0,0 +1,22 @@
+ let name = "Ayan"; 
+let age = 25;      
+
+
+const isStudent = true; 
+
+
+var subjects = ["Math", "Science", "English"]; 
+
+let studentInfo = {
+  id: 101,
+  name: "Ayan",
+  enrolled: true,
+  marks: [85, 90, 88]
+};
+
+// Logging all variables to the console
+console.log("Name:", name);               
+console.log("Age:", age);                 
+console.log("Is Student:", isStudent);    
+console.log("Subjects:", subjects);       
+console.log("Student Info:", studentInfo);
