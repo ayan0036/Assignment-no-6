727c63cdc3f29d26ec6125cd77c0bee4d92d4721
Pathan Ayan khan <ayankhan782204@gmail.com>
2025-06-09 12:16:55 +0530

    Create BasicTask4.js

---
+++ BasicTask4.js
@@ -0,0 +1,12 @@
+console.log("Numbers from 1 to 20:");
+for (let i = 1; i <= 20; i++) {
+  console.log(i);
+}
+
+// While loop: Print even numbers from 2 to 10
+console.log("Even numbers from 2 to 10:");
+let num = 2;
+while (num <= 10) {
+  console.log(num);
+  num += 2;
+}
